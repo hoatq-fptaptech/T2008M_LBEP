c09b28c5ed13ebffcd0a00c748513be8411c33ac
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-09-09 11:57:09 +0700

    Session 2

---
+++ Session2_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int a;
+	a = 10;
+	printf("a = %d",a);
+	a = a + 5;
+	printf("a = %d",a);
+	float pi = 3.1415;
+	printf("pi = %f",pi);
+	printf("a= %d va pi = %f va a+10= %d",a,pi,a+10);
+	int age;
+	printf("nhap tuoi cua ban:");
+	scanf("%d",&age);
+	
+	scanf("%f",&pi);
+}
---
+++ Session2_2.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int a,b,c;
+	printf("nhap a=");
+	scanf("%d",&a);
+	printf("nhap b=");
+	scanf("%d",&b);
+	printf("nhap c=");
+	scanf("%d",&c);
+	
+	if(a+b>c){
+		if(a+c>b){
+			if(b+c>a){
+				int p = a+b+c;
+				printf("%d %d %d la 3 canh tam giac\n",a,b,c);
+				printf("Chu vi: %d\n",p);
+				// tinh dien tich
+				float pi = 3.14158;
+				int x = (int)pi;
+				float p2 = (float)p;// ep kieu
+				p2 = p2/2;
+				float s = sqrt(p2*(p2-a)*(p2-b)*(p2-c));
+				printf("Dien tich: %f\n",s);
+			}else{
+				printf("%d %d %d ko phai la 3 canh tam giac",a,b,c);
+			}
+		}else{
+			printf("%d %d %d ko phai la 3 canh tam giac",a,b,c);
+		}
+	}else{
+		printf("%d %d %d ko phai la 3 canh tam giac",a,b,c);
+	}
+}
+

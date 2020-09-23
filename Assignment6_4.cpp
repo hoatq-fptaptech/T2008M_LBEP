ea3a8362179d473c4b1665f13557321d8c9e643f
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-09-23 08:41:52 +0700

    assignment 6

---
+++ Assignment6_4.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("Nhap n=");
+	scanf("%d",&n);
+	int arr[n];
+	
+	for(int i=0;i<n;i++){
+		printf("Nhap so thu %d: ",i);
+		scanf("%d",&arr[i]);
+	}
+	
+	int count = 0,max=0;
+	for(int i=0;i<n;i++){
+		if(arr[i]>0){
+			count+=arr[i];
+			if(count > max){
+				max = count;
+			}
+		}else{
+			count = 0;
+		}
+	}
+	printf("Tong so duong lien tiep nhieu nhat: %d",max);
+}
